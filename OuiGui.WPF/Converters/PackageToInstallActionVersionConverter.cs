7079d9ca5ce3081360a1cbbc52f8369363d9cc76
cmc13 <spectre13@gmail.com>
2015-02-01 10:28:20 -0600

    make version on install action view prettier

---
+++ OuiGui.WPF/Converters/PackageToInstallActionVersionConverter.cs
@@ -0,0 +1,44 @@
+﻿using OuiGui.Lib.Model;
+using OuiGui.WPF.Util;
+using System;
+using System.Text;
+using System.Windows.Data;
+
+namespace OuiGui.WPF.Converters
+{
+    public class PackageToInstallActionVersionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var installAction = value as InstallAction;
+            if (installAction != null)
+            {
+                var builder = new StringBuilder();
+                builder.Append('(');
+                if (installAction.Action == InstallActionType.Update)
+                {
+                    var package = installAction.Package as Package;
+                    if (package != null)
+                    {
+                        builder.Append('v')
+                            .Append(package.InstalledVersion)
+                            .Append(" => ");
+                    }
+                }
+
+                builder.Append('v')
+                    .Append(installAction.Package.Version)
+                    .Append(')');
+
+                return builder.ToString();
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
